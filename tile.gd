2b9dee0a8e69b018a30051606730eb113cf49478
noriim <vasssmarcelll@gmail.com>
2024-06-13 18:35:36 +0200

    working drag and drop test

--- tile.gd
+++ tile.gd
@@ -1,7 +1,6 @@
 extends ColorRect
 
 var mouse_in = false
-#signal change_color()
 
 func _input(event):
 	if event.is_action_pressed("click"):
@@ -14,10 +13,19 @@ func _input(event):
 				add_child(ball)
 				get_parent()._on_tile_change_color()
 				get_child(0).get_child(1).visible = get_parent().color_picker
-				#change_color.emit()
 
 func _on_mouse_entered():
 	mouse_in = true
 	
 func _on_mouse_exited():
 	mouse_in = false
+
+func _get_drag_data(at_position):
+	pass
+	
+func _can_drop_data(at_position, data):
+	return data is ColorRect
+	
+func _drop_data(at_position, data):
+	pass
+	
