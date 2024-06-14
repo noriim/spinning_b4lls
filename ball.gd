73f7e37f7207127d00299bc877d7a3b06a24ed7e
noriim <vasssmarcelll@gmail.com>
2024-06-14 16:11:48 +0200

    Adding and dragging merged complete, minors bug remain

--- ball.gd
+++ ball.gd
@@ -8,27 +8,48 @@ var offset: Vector2
 var initialPos: Vector2
 
 # Function to check if any sibling node is above the given node
-func which_sibling_above(node):
+func which_sibling_above(vector):
 	# Get the parent of the node
-	var siblings = node.get_parent().get_children()
-	
+	var siblings = get_parent().get_children()
 	# Get the global position of the node
-	var node_pos = node.global_position
-	print("node_pos:", node_pos)
+	var self_pos = vector
+	print("self_pos:", self_pos)
 	# Iterate through the parent's children
 	for sibling in siblings:
 		# Ensure we are not comparing the node with itself
-		if sibling != node and sibling is ColorRect:
+		if sibling != self and sibling is ColorRect:
 			var sibling_pos = sibling.global_position  + sibling.size / 2
 			print("sibling_pos:", sibling_pos)
-			# Check if the sibling is above the node
-			if sibling_pos.y-sibling.size.y < node_pos.y and sibling_pos.y+sibling.size.y > node_pos.y:
-				print("above")
+			# Check if the sibling is above the self
+			if (sibling_pos.x-sibling.size.x < self_pos.x 
+			and sibling_pos.x+sibling.size.x > self_pos.x 
+			and sibling_pos.y-sibling.size.y < self_pos.y 
+			and sibling_pos.y+sibling.size.y > self_pos.y):
+				print("rect above 2")
 				return sibling
 	print("none above 2")
 	return false
-	
 
+# Function to check if any sibling node is above the given node
+func is_ball_above(vector):
+	# Get the parent of the node
+	var siblings = get_parent().get_children()
+	# Get the global position of the node
+	var self_pos = vector
+	print("self_pos:", self_pos)
+	# Iterate through the parent's children
+	for sibling in siblings:
+		# Ensure we are not comparing the node with itself
+		if sibling != self and sibling is Node2D:
+			var sibling_pos = sibling.global_position
+			print("sibling_pos:", sibling_pos)
+			# Check if the sibling is above the self
+			if (sibling_pos.x == self_pos.x  
+			and sibling_pos.y == self_pos.y ):
+				print("ball above 2")
+				return sibling
+	print("none above 2")
+	return false
 
 func _process(delta):
 	if draggable:
@@ -36,23 +57,28 @@ func _process(delta):
 			initialPos = global_position
 			offset = get_global_mouse_position() - global_position
 			global.is_dragging = true
-			if which_sibling_above(self):
-				which_sibling_above(self).visible = true
+			if which_sibling_above(self.global_position):
+				which_sibling_above(self.global_position).visible = false
 		if Input.is_action_pressed("click"):
 			global_position = get_global_mouse_position() - offset
 		elif Input.is_action_just_released("click"):
 			global.is_dragging = false
-			if which_sibling_above(self):
-				which_sibling_above(self).visible = false
+			if which_sibling_above(self.global_position):
+				which_sibling_above(self.global_position).visible = false
 			var tween = get_tree().create_tween()
-			if is_inside_dropable:
+			if is_inside_dropable and not is_ball_above(body_ref.global_position):
 				tween.tween_property(self, "position", body_ref.global_position, 0.2).set_ease(Tween.EASE_OUT)
+				print("TEST")
+				if initialPos != body_ref.global_position:
+					which_sibling_above(initialPos).visible = true
 			else:
 				tween.tween_property(self, "global_position", initialPos, 0.2).set_ease(Tween.EASE_OUT)
+				print("TEST2")
+				which_sibling_above(initialPos).visible = false
 
 func clear_behind_balls():
-	if which_sibling_above(self):
-					which_sibling_above(self).visible = false
+	if which_sibling_above(self.global_position):
+					which_sibling_above(self.global_position).visible = false
 
 
 func _on_area_2d_mouse_entered():
